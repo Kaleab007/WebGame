d88dc151786822ad943021c0bfc69629219dbcde
Kaleab007 <abatenehkaleab@gmail.com>
2022-03-02 18:40:09 -0800

    canvas

--- app.js
+++ app.js
@@ -7,12 +7,15 @@
     maxWidth=20;
     minGap=200;
     maxGap=500;
-
+var myObstacles=[];
 function startGame () {
     gamearea.start();
-    var myObstacles=[];
+    
 }
-//dimensionsize
+function everyinterval(n){
+    if(gamearea.frame);
+}
+//dimension.size
 function obstacle()
 this.height=Math.floor(minHeight+Math.random()*(maxHeight-minHeight+1));
 this.width=Math.floor(maxWidth+Math.random()*(maxWidth-minWidth +1));
@@ -29,16 +32,20 @@ var gamearea = {
         this.canvas.width=1000;
         //obstacle.intervals 
         document.body.insertBefore(this.canvas,document.body.childNodes[0]);
-        myObstacles.push(new obstacle());
+       this.frame=0;
         this.context=this.canvas.getContext("2d");
         this.interval=setInterval(this.updateGameArea,4);
 
      },
      updateGameArea: function(){
-         myObstacles[0].x=-1;
-         myObstacles[0].draw()
-
-}, 
+         gamearea.clear();
+         if(everyinterval (150)){
+             myObstacles.push(new obstacle());
+         }
+         for(i=0; i<myObstacles.length;i++){
+         myObstacles[i].x=-1;
+         myObstacles[i].draw()}
+        }, 
 clear:function (){
     gamearea.context.clearRect(0,0, this.canvas.width,this.canvas.width);
 },
